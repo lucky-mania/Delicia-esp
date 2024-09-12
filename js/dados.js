63dd7b75da60396ba770b4bc2da4edb28d45cae6
lucky-mania <joeaneto@gmail.com>
2024-09-12 11:23:16 -0300

    ss

--- js/dados.js
+++ js/dados.js
@@ -2,89 +2,12 @@ var MENU = {
     "churrasco": [
         {
             "id": "ribs-brisket-and-burnt-ends",
-            "img": "./img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
-            "name": "Joe's KC BBQ",
+            "img": "img/bolo-removebg-preview.png",
+            "name": "Bolo cobertura",
             "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
-            "price": 110.99
+            "price": 10
         },
     
-        {
-            "id": "005-kings-carolina-oink-sampler",
-            "img": "./img/cardapio/churrasco/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg",
-            "name": "Kings BBQ",
-            "dsc": "Carolina BBQ Oink Sampler",
-            "price": 89
-        },
-        {
-            "id": "texas-monthlys-1-bbq-brisket",
-            "img": "./img/cardapio/churrasco/texas-monthlys-1-bbq-brisket.1006a061be7acae03992e420fbca995a.jpg",
-            "name": "Snow's BBQ",
-            "dsc": "Texas Monthly's #1 BBQ Brisket",
-            "price": 199
-        },
-        {
-            "id": "whole-brisket-texas-bbq-sauce",
-            "img": "./img/cardapio/churrasco/whole-brisket-texas-barbecue-bbq-sauce.e07ee4818b20ed43d217bf67fedd41ce.jpg",
-            "name": "Franklin Barbecue",
-            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
-            "price": 249
-        },
-        {
-            "id": "whole-texas-smoked-brisket",
-            "img": "./img/cardapio/churrasco/whole-texas-smoked-brisket.a5558a25381e271408e197936e7985d8.jpg",
-            "name": "Terry Black's Barbecue",
-            "dsc": "Whole Texas Smoked Brisket",
-            "price": 189
-        },
-        {
-            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
-            "img": "./img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
-            "name": "Bludso's BBQ",
-            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
-            "price": 139
-        },
-        {
-            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
-            "img": "./img/cardapio/churrasco/blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo.a95a3e632ae324f719738a2a5c1dff6e.jpg",
-            "name": "Blue Smoke",
-            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
-            "price": 129
-        },
-        {
-            "id": "006-kings-meat-lovers-special",
-            "img": "./img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
-            "name": "Kings BBQ",
-            "dsc": "BBQ Meat Lovers Special for 10",
-            "price": 139
-        },
-        {
-            "id": "the-big-ugly-bbq-dinner-for-6",
-            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
-            "name": "Ugly Drum",
-            "dsc": "The Big Ugly BBQ Dinner for 6",
-            "price": 229
-        },
-        {
-            "id": "17796-mighty-quinns-bbq-sampler-pack",
-            "img": "./img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
-            "name": "Mighty Quinn's BBQ",
-            "dsc": "Mighty Quinn's BBQ Sampler Pack",
-            "price": 169
-        },
-        {
-            "id": "post-oak-smoked-half-brisket",
-            "img": "./img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
-            "name": "Southside Market & Barbeque",
-            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
-            "price": 109
-        },
-        {
-            "id": "best-of-texas-bbq-combo-serves-14",
-            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
-            "name": "Snow's BBQ",
-            "dsc": "Best of Texas BBQ Combo - Serves 14",
-            "price": 269
-        }
     ],
     "burgers": [
         {
@@ -219,88 +142,61 @@ var MENU = {
     "bebidas": [
         {
             "id": "hong-kong-boba-tea-kit-for-6",
-            "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
-            "name": "New Territories",
+            "img": "img/donnutsem-removebg-preview.png",
+            "name": "2 mini donut",
             "dsc": "Hong Kong Boba Tea Kit for 6",
             "price": 59
         },
         {
             "id": "guys-caliente-margaritas-for-12",
-            "img": "./img/cardapio/bebidas/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
-            "name": "Guy Fieri",
+            "img": "img/donnutsem-removebg-preview.png",
+            "name": "Caixa 6unid",
             "dsc": "Guy's Caliente Margaritas for 12",
             "price": 69
         },
         {
             "id": "woodford-reserve-mint-julep-syrup",
-            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
-            "name": "Woodford Reserve",
+            "img": "img/donnutsem-removebg-preview.png",
+            "name": "Caixa 12unid",
             "dsc": "Woodford Reserve Mint Julep Syrup",
             "price": 39
         },
         {
             "id": "new-orleans-hurricane-mix",
-            "img": "./img/cardapio/bebidas/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
-            "name": "Franco's Hurricane Mix",
+            "img": "img/donnutsem-removebg-preview.png",
+            "name": "caixa 25unid",
             "dsc": "New Orleans Hurricane Mix",
             "price": 39
         },
         {
             "id": "margarita-mix",
-            "img": "./img/cardapio/bebidas/margarita-mix.bd48a000d589d3147b14790af3c33fcd.jpg",
-            "name": "Johnny Sanchez",
+            "img": "img/donnutsem-removebg-preview.png",
+            "name": "meio centro",
             "dsc": "Margarita Mix",
             "price": 59
         },
         {
             "id": "woodford-reserve-mint-julep-syrup-2-pack",
-            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup-2-pack.0ac76063f151988113cbaabd0eaa829f.jpg",
-            "name": "Woodford Reserve",
+            "img": "img/donnutsem-removebg-preview.png",
+            "name": "centro",
             "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
             "price": 59
         },
         {
             "id": "unicorn-parade-milkshake-kit-for-8",
-            "img": "./img/cardapio/bebidas/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
+            "img": "img/donnutsem-removebg-preview.png",
             "name": "New Territories",
             "dsc": "Unicorn Parade Milkshake Kit for 8",
             "price": 109
         },
         {
             "id": "chickpea-chiller-kit-for-6",
-            "img": "./img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
+            "img": "img/donnutsem-removebg-preview.png  ",
             "name": "The Hummus & Pita Co.",
             "dsc": "Chickpea Chiller Kit for 6",
             "price": 89
         },
-        {
-            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
-            "img": "./img/cardapio/bebidas/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
-            "name": "Old Honey Barn Mint Julep",
-            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
-            "price": 25
-        },
-        {
-            "id": "kentucky-derby-mint-julep-gift-set",
-            "img": "./img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
-            "name": "Woodford Reserve",
-            "dsc": "Kentucky Derby Mint Julep Gift Set",
-            "price": 59.95
-        },
-        {
-            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
-            "img": "./img/cardapio/bebidas/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
-            "name": "Charleston Beverage Company",
-            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
-            "price": 39.95
-        },
-        {
-            "id": "nola-cold-brew-concentrate-bag-in-box",
-            "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
-            "name": "Grady's Cold Brew",
-            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
-            "price": 40
-        }
+
     ],
     "pizzas": [
         {
@@ -328,87 +224,45 @@ var MENU = {
     "steaks": [
         {
             "id": "california-reserve-filet-mignon-steaks-gift-box",
-            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
-            "name": "Flannery Beef",
+            "img": "img/donut-removebg-preview.png",
+            "name": "2 mini donut",
             "dsc": "California Reserve Filet Mignon Steaks Gift Box",
-            "price": 129
+            "price": 2
         },
         {
             "id": "steaks-and-cakes-date-night-dinner-for-2",
-            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
-            "name": "Chesapeake Bay Gourmet",
+            "img": "img/donut-removebg-preview.png",
+            "name": "Caixa 6unid",
             "dsc": "Steaks and Cakes Date Night Dinner for 2",
-            "price": 129
+            "price": 4
         },
         {
             "id": "Prime-holiday-steak-sampler-for-10-12",
-            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
-            "name": "Saltbrick Prime",
+            "img": "img/donut-removebg-preview.png",
+            "name": "Caixa 12unid",
             "dsc": "Chef Matt's Steak Sampler for 10-12",
-            "price": 179
+            "price": 8
         },
         {
             "id": "bone-in-rib-steak",
-            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
-            "name": "Old Homestead Steakhouse",
+            "img": "img/donut-removebg-preview.png",
+            "name": "caixa 25unid",
             "dsc": "Bone-in Rib Steak",
-            "price": 159
+            "price": 38
         },
         {
             "id": "american-wagyu-gold-grade-top-sirloins",
-            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
-            "name": "Snake River Farms",
+            "img": "img/donut-removebg-preview.png",
+            "name": "meio centro",
             "dsc": "American Wagyu Gold Grade Top Sirloins",
-            "price": 119
+            "price": 85
         },
         {
             "id": "2-peter-luger-steak-pack-b",
-            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
-            "name": "Peter Luger Steak House",
+            "img": "img/donut-removebg-preview.png",
+            "name": "centro",
             "dsc": "Peter Luger Porterhouse Steaks",
-            "price": 215.95
+            "price": 135
         },
-        {
-            "id": "ribeye-prime-steak-gift-box",
-            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
-            "name": "Churchill's Steakhouse",
-            "dsc": "Ribeye Prime Steak Gift Box",
-            "price": 229
-        },
-        {
-            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
-            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
-            "name": "Pat LaFrieda Meats",
-            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
-            "price": 96.7
-        },
-        {
-            "id": "california-reserve-ribeye-steak",
-            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
-            "name": "Flannery Beef",
-            "dsc": "California Reserve Ribeye Steak - 12 oz",
-            "price": 32
-        },
-        {
-            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
-            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
-            "name": "Chef Francis Mallmann",
-            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
-            "price": 225
-        },
-        {
-            "id": "california-reserve-filet-mignon-steak",
-            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
-            "name": "Flannery Beef",
-            "dsc": "California Reserve Filet Mignon Steak",
-            "price": 22
-        },
-        {
-            "id": "mesquite-smoked-peppered-beef-tenderloin",
-            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
-            "name": "Perini Ranch Steakhouse",
-            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
-            "price": 165
-        }
     ]
 }
